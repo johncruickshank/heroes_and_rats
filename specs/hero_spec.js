a02e2dff899fb1c089b989d29a418a14a7e28149
Alistair Kane <vplelmo@hotmail.co.uk>
2017-11-30 15:37:00 +0000

    adds the start of hero spec

---
+++ specs/hero_spec.js
@@ -0,0 +1,11 @@
+var assert = require('assert');
+var Hero = require('./hero.js');
+var Task = require('./task.js');
+
+describe("Hero", function(){
+    var hero;
+
+    beforeEach(function(){
+        hero = new Hero()
+    });
+})
